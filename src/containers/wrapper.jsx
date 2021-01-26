dc0349f570b44b7203e0f3246590fc2dfa1c6aea
Alena Gritsiuk <alena22292@mail.ru>
2021-01-26 11:23:58 +0100

    Add style to the nav

--- src/containers/wrapper.jsx
+++ src/containers/wrapper.jsx
@@ -5,11 +5,11 @@ class Wrapper extends Component {
     return (
      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Fixed navbar</a>
+          <a className="navbar-brand" href="#">MyPortfolio</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarCollapse">
+          <div className="collapse navbar-collapse d-flex justify-content-between" id="navbarCollapse">
             <ul className="navbar-nav me-auto mb-2 mb-md-0">
               <li className="nav-item active">
                 <a className="nav-link" aria-current="page" href="#">Home</a>
