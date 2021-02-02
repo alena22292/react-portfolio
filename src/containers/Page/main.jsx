2146b7f9337dd3373ffa27a3412342a6949203dd
Alena Gritsiuk <alena22292@mail.ru>
2021-02-02 11:08:36 +0100

    Add font size to each h-range

--- src/containers/Page/main.jsx
+++ src/containers/Page/main.jsx
@@ -3,11 +3,28 @@ import React, { Component } from 'react';
 class Main extends Component {
   render() {
     return (
-      <section>
-        <div>
-          <h1>This is main content</h1>
-        </div>
-      </section>
+      <div>
+        <section class="s1">
+          <div class="main-container">
+            <h1>This is main content</h1>
+          </div>
+        </section>
+        <section class="s2">
+          <div class="main-container">
+            <h1>About</h1>
+          </div>
+        </section>
+        <section class="s1">
+          <div class="main-container">
+            <h1>Contact</h1>
+          </div>
+        </section>
+        <section class="s2">
+          <div class="main-container">
+            <h1>Portfolio</h1>
+          </div>
+        </section>
+      </div>
     );
   }
 }
